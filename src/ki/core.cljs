bae972c5b5574901bee6c3c162a55763f000c12c
Alexis Vincent <mail@alexisvincent.io>
2021-04-15 15:41:23 +0100

    v0.0.3

--- src/ki/core.cljs
+++ src/ki/core.cljs
@@ -1,4 +1,5 @@
 (ns ki.core
+  (:require-macros ki.core)
   (:require [uix.core.alpha :as uix]))
 
 (defn browser? []
@@ -7,3 +8,7 @@
 (defn use-state [default]
   (let [s* (uix/state default)]
     [@s* #(reset! s* %)]))
+
+(defn use-effect!
+  ([f] (use-effect! f))
+  ([f deps] (uix/effect! f deps)))
